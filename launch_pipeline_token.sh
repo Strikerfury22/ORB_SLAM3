c41cde540ab1bf6a5a6f47d026eec9cd3c9f4b2e
jorge <682971@unizar.es>
2023-11-29 01:25:07 +0100

    Don't copy Frame

--- launch_pipeline_token.sh
+++ launch_pipeline_token.sh
@@ -5,11 +5,3 @@ do
     echo NUMBER TOKENS $i
     ./launch_missing.sh Results/pipeline_token_parallel/$i 3 $i 
 done
-
-./build.sh
-
-for i in {25..30}
-do
-    echo NUMBER TOKENS $i
-    ./launch_missing.sh Results/pipeline_token_thread/$i 3 $i 
-done
