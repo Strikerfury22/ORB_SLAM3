12c23433d4be71f1c75beeea6ad237d9c7aab51b
jorge <682971@unizar.es>
2023-12-03 17:27:47 +0100

    Experiment

--- launch_grainsize.sh
+++ launch_grainsize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 grainsize=1
-max_value=2500
+max_value=1500
 until ((grainsize > max_value)); do
     echo $grainsize
-    ./launch_missing.sh Results/grainsize/$grainsize 3 $grainsize
+    ./launch_missing.sh Results/grainsizePF1/$grainsize 3 $grainsize
     grainsize=$((2*grainsize))
 done
